ec77346c22113869b6d1c74444e535d050015c3a
KMininkov <mininkov3228@gmail.com>
2021-04-20 13:25:51 +0300

    fixed, added validations

--- crud.py
+++ crud.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import mysql.connector
+from marshmallow import fields, validate, exceptions
 import json
-import copy
 
-with open('secret.json') as f:
+with open('SECRET.json') as f:
     SECRET = json.load(f)
 
     DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}".format(
@@ -26,7 +25,7 @@ ma = Marshmallow(app)
 
 class SmartDevice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Float, unique=False)
+    price = db.Column(db.Integer, unique=False)
     operating_voltage_in_watts = db.Column(db.Float, unique=False)
     current_consumption_in_watts = db.Column(db.Float, unique=False)
     model_name = db.Column(db.String(32), unique=False)
@@ -40,11 +39,28 @@ class SmartDevice(db.Model):
         self.model_name = model_name
         self.manufacturer = manufacturer
 
+    def update(self, price, operating_voltage_in_watts, current_consumption_in_watts, model_name, manufacturer):
+        self.__init__(price, operating_voltage_in_watts, current_consumption_in_watts, model_name, manufacturer)
+
+
+def get_device_by_id(id):
+    smart_device = SmartDevice.query.get(id)
+    if smart_device is None:
+        return abort(404)
+    return smart_device
+
+
+@app.errorhandler(exceptions.ValidationError)
+def handle_exception(e):
+    return e.messages, 400
+
 
 class SmartDeviceSchema(ma.Schema):
-    class Meta:
-        fields = ('price', 'operating_voltage_in_watts', 'current_consumption_in_watts', 'model_name',
-                  'manufacturer', 'bluetooth_connection')
+    price = fields.Integer(validate=validate.Regexp)
+    operating_voltage_in_watts = fields.Float(validate=validate.Range(0.1, 10000))
+    current_consumption_in_watts = fields.Float(validate=validate.Range(0.1, 10000))
+    model_name = fields.String(validate=validate.Length(max=80))
+    manufacturer = fields.String(validate=validate.Length(max=20))
 
 
 smart_device_schema = SmartDeviceSchema()
@@ -53,26 +69,25 @@ smart_devices_schema = SmartDeviceSchema(many=True)
 
 @app.route("/smart_device", methods=["POST"])
 def add_smart_device():
-    smart_device = SmartDevice(request.json['price'],
-                               request.json['operating_voltage_in_watts'],
-                               request.json['current_consumption_in_watts'],
-                               request.json['model_name'],
-                               request.json['manufacturer'])
-    db.session.add(smart_device)
+    fields = smart_device_schema.load(request.json)
+    new_smart_device = SmartDevice(**fields)
+
+    db.session.add(new_smart_device)
     db.session.commit()
-    return smart_device_schema.jsonify(smart_device)
+
+    return smart_device_schema.jsonify(new_smart_device)
 
 
 @app.route("/smart_device", methods=["GET"])
 def get_smart_device():
-    all_smart_device = SmartDevice.query.all()
-    result = smart_devices_schema.dump(all_smart_device)
+    all_smart_devices = SmartDevice.query.all()
+    result = smart_devices_schema.dump(all_smart_devices)
     return jsonify(result)
 
 
 @app.route("/smart_device/<id>", methods=["GET"])
 def smart_device_detail(id):
-    smart_device = SmartDevice.query.get(id)
+    smart_device = get_device_by_id(id)
     if not smart_device:
         abort(404)
     return smart_device_schema.jsonify(smart_device)
@@ -80,29 +95,29 @@ def smart_device_detail(id):
 
 @app.route("/smart_device/<id>", methods=["PUT"])
 def smart_device_update(id):
-    smart_device = SmartDevice.query.get(id)
+    smart_device = get_device_by_id(id)
+    fields = smart_device_schema.load(request.json)
+    smart_device.update(**fields)
+
     if not smart_device:
         abort(404)
-    old_smart_device = copy.deepcopy(smart_device)
-    smart_device.price = request.json['price']
-    smart_device.operating_voltage_in_watts = request.json['operating_voltage_in_watts']
-    smart_device.current_consumption_in_watts = request.json['current_consumption_in_watts']
-    smart_device.model_name = request.json['model_name']
-    smart_device.manufacturer = request.json['manufacturer']
-    db.session.commit()
-    return smart_device_schema.jsonify(old_smart_device)
 
-
-@app.route("/smart_device/<id>", methods=["DELETE"])
-def smart_device_delete(id):
-    smart_device = SmartDevice.query.get(id)
-    if not smart_device:
-        abort(404)
-    db.session.delete(smart_device)
     db.session.commit()
     return smart_device_schema.jsonify(smart_device)
 
 
+@app.route("/smart_device/<id>", methods=["DELETE"])
+def smart_device_delete(id):
+    smart_device = get_device_by_id(id)
+
+    if not smart_device:
+        abort(404)
+
+    db.session.delete(smart_device)
+    db.session.commit()
+
+    return smart_device_schema.jsonify(smart_device)
+
+
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True)
